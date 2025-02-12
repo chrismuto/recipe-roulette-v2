5fc92310d5ba71e59ca99d3cd4297b5f0fb1af29
chrismuto <chrismuto@gmail.com>
2025-02-12 11:22:33 -0800

    built out header and video/image components, added fetch call to api

---
+++ src/components/Image.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import recipe from '../App.jsx'
+
+export default function Image(props) {
+
+    const [youTubeUrl, setYouTubeUrl] = useState("")
+
+    console.log(props.youTubeUrl, typeof(props.youTubeUrl))
+    useEffect (() => {
+        if (props.youTubeUrl !== "") {
+            var firstSplit = props.youTubeUrl.split("watch");
+            var secondSplit = firstSplit[1].split("=");
+            setYouTubeUrl(firstSplit[0] + "embed/" + secondSplit[1]);
+        }
+    }, [props.youTubeUrl]);
+
+    function Video() {
+        return (
+            <iframe title="youtube video" src={youTubeUrl}></iframe>
+        )
+    }
+
+    function Image() {
+        return <img src={props.thumbNail}></img>
+    }
+
+    return (
+        <div>
+            {props.youTubeUrl !== "" ?  <Video /> : <Image />}
+        </div>
+    )
+}
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+export default function Title(props) {
+
+    return (
+        <div>
+            <h2>{props.title}</h2>
+            <h3>{props.nationality}</h3>
+        </div>
+    )
+}
