3a373920ad51769401b1c771a29cdb5543b22433
chrismuto <chrismuto@gmail.com>
2025-03-17 18:19:11 -0700

    added paragraphs and styling for recipe to break up large block of text

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react"
+
 export default function Recipe(props) {
+    const [splitRecipe, setSplitRecipe] = useState([]);
+    const [formattedRecipe, setFormattedRecipe] = useState("");
+
+    useEffect(() => {
+        if (props.recipe) setSplitRecipe(props.recipe.split("\n"))
+    }, [props.recipe])
+
+    useEffect(() => {
+        if (splitRecipe) setFormattedRecipe(splitRecipe.map(step => {return <div className="recipe-step">{step}</div>}))
+    }, [splitRecipe])
 
     return (props.recipe &&
         <div className="recipe">
+            {formattedRecipe}
             <hr className="horizontal-break" />
-            {props.recipe}
         </div>
     )
 }
